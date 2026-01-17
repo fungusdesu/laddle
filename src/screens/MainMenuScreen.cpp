51416ed538b6cf72790810b860ddf51aeb523a06
trantonminhky <fungusvalentinealt69420@gmail.com>
2026-01-17 15:34:48 +0700

    wrote L A D D L E on main menu

--- src/screens/MainMenuScreen.cpp
+++ src/screens/MainMenuScreen.cpp
@@ -1,4 +1,10 @@
+#include <iostream>
+
 #include "screens/MainMenuScreen.hpp"
+
+#include "helper/centerTextInRect.hpp"
+
+constexpr auto LADDLE_TEXT_Y_POSITION = 100.0f;
 
 void MainMenuScreen::handleInput(const sf::Event& event)
 {
@@ -13,7 +19,17 @@ void MainMenuScreen::update()
 void MainMenuScreen::draw()
 {
 	const sf::Font& font = GameContext::getFont("VCR_OSD_MONO");
+
 	sf::Text laddleText(font, "LADDLE", 120);
+	{
+		laddleText.setLetterSpacing(2.0f);
+		auto lbounds = laddleText.getLocalBounds();
+		laddleText.setOrigin(lbounds.position + lbounds.size / 2.0f);
+		laddleText.setPosition({p_window->getSize().x / 2.0f, LADDLE_TEXT_Y_POSITION});
+	}
+	
+	sf::Text playText(font, "PLAY");
+
 
 	p_window->clear();
 	p_window->draw(laddleText);
